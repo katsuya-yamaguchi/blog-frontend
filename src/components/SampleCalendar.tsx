35c1e207eb6148f1484a266901bf60228824fa07
katsuya yamaguchi <>
2020-07-20 14:45:29 +0900

    Add: イベント登録用フォームの作成。
    Formの作成は勿論、FullCalendarコンポーネントでのイベントクリック時or日付選択時の処理を行う空の関数も作成。(関数の実装は後続作業で行う。)

--- src/components/SampleCalendar.tsx
+++ src/components/SampleCalendar.tsx
@@ -1,13 +1,201 @@
-import React from 'react'
+import React, {useState} from 'react'
 import FullCalendar from '@fullcalendar/react'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import interactionPlugin from '@fullcalendar/interaction'
+import DatePicker, { registerLocale } from "react-datepicker";
+import ja from 'date-fns/locale/ja'
+import {createStyles, makeStyles} from "@material-ui/core/styles";
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    cover: {
+      opacity: 0,
+      visibility: 'hidden',
+      position: 'fixed',
+      width: '100%',
+      height: '100%',
+      zIndex: 1000,
+      top: 0,
+      left: 0
+    },
+    cover_inview: {
+      opacity: 1,
+      visibility: 'visible',
+      background: 'rgba(0, 0, 0, 0.3)'
+    },
+    form: {
+      opacity: 0,
+      visibility: 'hidden',
+      position: 'fixed',
+      top: 0,
+      left: 0,
+      fontWeight: 'bold',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    form_inview: {
+      opacity: 1,
+      visibility: 'visible',
+      background: 'rgba(255, 255, 255)',
+      width: '400px',
+      height: '300px',
+      zIndex: 2000,
+      top: '30%',
+      left: '40%'
+    }
+  })
+)
+
+registerLocale('ja', ja)
 
 const SampleCalendar: React.FC = props => {
+  const classes = useStyles()
+
+  /**
+   * 予定を追加する際にCalendarオブジェクトのメソッドを使用する必要がある。
+   * (CalendarオブジェクトはRef経由でアクセスする必要がある。)
+   */
+  const ref = React.createRef<any>()
+
+  const [inputTitle, setInputTitle] = useState('') // フォームに入力されたタイトル。
+  const [inputStart, setInputStart] = useState(new Date) // イベントの開始時刻。
+  const [inputEnd, setInputEnd] = useState(new Date) // イベントの終了時刻。
+  const [inView, setInView] = useState(false) // イベント登録フォームの表示有無。(trueなら表示する。)
+
+  /**
+   * カレンダーがクリックされた時にイベント登録用のフォームを表示する。
+   * それぞれのフォームが下記の状態で表示される。
+   *  - タイトル: 空欄
+   *  - 開始: クリックしたカレンダーの開始時間
+   *  - 終了: クリックしたカレンダーの終了時間
+   */
+  const handleCLick = () => {
+    setInView(true)
+  }
+
+  /**
+   * カレンダーから登録された予定をクリックした時にイベント変更用のフォームを表示する。
+   * それぞれのフォームが下記の状態で表示される。
+   *  - タイトル: 選択した予定のタイトル
+   *  - 開始: 選択した予定の開始時間
+   *  - 終了: 選択した予定の終了時間
+   */
+  const handleSelect = () => {
+    setInView(true)
+  }
+
+  /**
+   * カレンダーに予定を追加する。
+   */
+  const onAddEvent = () => {}
+
+  /**
+   * フォームが表示された時に、グレー背景でフォーム以外を非アクティブ化に見えるようにするための要素。
+   */
+  const coverElement = (
+    <div
+      onClick={() => setInView(false)}
+      className={
+        inView
+          ? `${classes.cover} ${classes.cover_inview}`
+          : classes.cover
+      }
+    />
+  )
+
+  const titleElement = (
+    <div>
+      <label>タイトル</label>
+      <input
+        type="text"
+        value={inputTitle}
+        name="inputTitle"
+        onChange={e => {
+          setInputTitle(e.target.value)
+        }}
+      />
+    </div>
+  )
+
+  const startTimeElement = (
+    <div>
+      <label>開始</label>
+      <DatePicker
+        locale="ja"
+        dateFormat="yyyy/MM/d HH:mm"
+        selected={inputStart}
+        showTimeSelect
+        timeFormat="HH:mm"
+        timeIntervals={10}
+        todayButton="today"
+        name="inputStart"
+        onChange={(time: Date) => {
+          setInputStart(time)
+        }}
+      />
+    </div>
+  )
+
+  const endTimeElement = (
+    <div>
+      <label>終了</label>
+      <DatePicker
+        locale="ja"
+        dateFormat="yyyy/MM/d HH:mm"
+        selected={inputEnd}
+        showTimeSelect
+        timeFormat="HH:mm"
+        timeIntervals={10}
+        todayButton="today"
+        name="inputEnd"
+        onChange={(time: Date) => {
+          setInputEnd(time)
+        }}
+      />
+    </div>
+  )
+
+  const btnElement = (
+    <div>
+      <input
+        type="button"
+        value="キャンセル"
+        onClick={() => {
+          setInView(false)
+        }}
+      />
+      <input
+        type="button"
+        value="保存"
+        onClick={() => onAddEvent()}
+      />
+    </div>
+  )
+
+  const formElement = (
+    <div
+      className={
+        inView
+          ? `${classes.form} ${classes.form_inview}`
+          : classes.form
+      }
+    >
+      <form>
+        <div>予定を入力</div>
+        {titleElement}
+        {startTimeElement}
+        {endTimeElement}
+        {btnElement}
+      </form>
+    </div>
+  )
 
   return (
     <div>
+      {coverElement}
+      {formElement}
       <FullCalendar
         locale="ja"
         plugins={[timeGridPlugin, dayGridPlugin, interactionPlugin]}
@@ -29,6 +217,9 @@ const SampleCalendar: React.FC = props => {
           center: 'prev, next, today',
           end: 'dayGridMonth,timeGridWeek'
         }}
+        ref={ref}
+        eventClick={handleCLick}
+        select={handleSelect}
       />
     </div>
   )
